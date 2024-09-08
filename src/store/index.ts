e29e604c71c45eb2e35be7fcefc2230f2154badb
atryomkorshykau <butovichjunior@gmail.com>
2024-09-08 14:34:13 +0300

    create store

---
+++ src/store/index.ts
@@ -0,0 +1,21 @@
+import GlobalHook from 'use-global-hook'
+import appState from '@/store/states/appState/appState'
+import appActions from '@/store/actions/appActions/appActions'
+import { Actions, State } from '@/store/types'
+
+
+const states: State = {
+  
+  appState: appState
+  
+}
+
+const actions = {
+  
+  appActions: appActions
+  
+}
+
+const useGlobal = GlobalHook<State, Actions>( states, actions )
+
+export default useGlobal
