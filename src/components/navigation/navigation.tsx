ae248b1579f42921e46b7c1d1c3a73efd6df43e2
atryomkorshykau <butovichjunior@gmail.com>
2024-09-08 14:47:49 +0300

    update build

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,7 +1,6 @@
 import style from './navigation.module.scss'
 import Name from '@/shared/name/name'
 import BurgerMenu from '@/shared/burger.menu/ui/burger.menu'
-import useGlobal from '@/store'
 
 const Navigation = () => {
   
