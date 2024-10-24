4fde51beae8bd99ca4a9bfecf28a71c020620b3a
artyomkorshykau <butovichjunior@gmail.com>
2024-10-24 11:31:13 +0300

    fix adaptive burger

--- src/shared/burger.menu/ui/burger.menu.tsx
+++ src/shared/burger.menu/ui/burger.menu.tsx
@@ -2,13 +2,12 @@
 
 import React from 'react'
 import styles from './burger.menu.module.scss'
-import useGlobal from '@/store'
+import { useAppStore } from '@/store/app-store'
+
 
 const BurgerMenu = () => {
   
-  const [ globalState, globalActions ] = useGlobal()
-  const { isOpenMenu } = globalState.appState
-  const { toggleMenu } = globalActions.appActions
+  const { setToggleMenu, toggleMenu } = useAppStore()
   
   return (
     
@@ -16,54 +15,54 @@ const BurgerMenu = () => {
       
       <input
         
-        className = { styles.checkbox }
-        type = "checkbox"
-        checked = { isOpenMenu }
-        onChange = { () => toggleMenu() }
+        className={ styles.checkbox }
+        type="checkbox"
+        checked={ toggleMenu }
+        onChange={ () => setToggleMenu() }
       
       />
       
       <svg
         
-        fill = { 'none' }
-        viewBox = "0 0 50 50"
-        height = "50"
-        width = "50"
+        fill={ 'none' }
+        viewBox="0 0 50 50"
+        height="50"
+        width="50"
       
       >
         
         <path
           
-          className = { `${ styles.lineTop } ${ styles.line }` }
-          strokeLinecap = "round"
-          strokeWidth = "3"
-          stroke = "black"
-          d = "M6 11L44 11"
+          className={ `${ styles.lineTop } ${ styles.line }` }
+          strokeLinecap="round"
+          strokeWidth="3"
+          stroke="black"
+          d="M6 11L44 11"
         
         />
         
         <path
           
-          className = { `${ styles.lineMid } ${ styles.line }` }
-          strokeLinecap = "round"
-          strokeWidth = "3"
-          stroke = "black"
-          d = "M6 24H43"
+          className={ `${ styles.lineMid } ${ styles.line }` }
+          strokeLinecap="round"
+          strokeWidth="3"
+          stroke="black"
+          d="M6 24H43"
         
         />
         
         <path
           
-          className = { `${ styles.lineBottom } ${ styles.line }` }
-          strokeLinecap = "round"
-          strokeWidth = "3"
-          stroke = "black"
-          d = "M6 37H43"
+          className={ `${ styles.lineBottom } ${ styles.line }` }
+          strokeLinecap="round"
+          strokeWidth="3"
+          stroke="black"
+          d="M6 37H43"
         
         />
       
       </svg>
-      
+    
     </div>
   
   )
